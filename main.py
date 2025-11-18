9b0c9277c2d628569137ed066986cf1e1f74029d
Görkem <smailgorkem@gmail.com>
2025-11-18 08:38:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ from user import User
 from admin import Admin
 from bank_operations import Bank
 
-print("Hello Worlds")
+#print("Hello Worlds")
 
-file_user_data = open("user_data.json")
-contents = file_user_data.read()
-print(contents)
+#file_user_data = open("user_data.json")
+#contents = file_user_data.read()
+#print(contents)
 
-with open("history.json") as file:
-    history2 = file.read()
-    print (history2)
+#with open("history.json") as file:
+#    history2 = file.read()
+#    print (history2)
 
-with open("history.json", mode="w") as histt5:
-    histt5.write("\nNew text.")
+#with open("history.json", mode="w") as histt5:
+#    histt5.write("\nNew text.")
+
+while True:
+    print("----------------------------------------")
+    print("Welcome to Paradise Bank Online")
+    print("----------------------------------------")
+    print("-----------(1)--> User Login <----------")
+    print("-----------(2)--> Register <------------")
+    print("-----------(3)--> Produce Password <----")
+    print("-----------(4)--> Forgot Password <-----")
+    print("-----------(5)--> Support from AI <-----")
+    print("----------------------------------------")
+    menu_select = input("")
